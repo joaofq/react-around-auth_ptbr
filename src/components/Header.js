3ce3e317b42fccad9a16dbdb3ce4cc727a0a6631
João Fernando <joaofq@hotmail.com>
2022-11-18 21:58:10 -0300

    mend

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import { Children } from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../images/logo.svg';
 
 function Header({ children }) {
   return (
     <header className="header">
-      <img src={logo} alt="Around the U.S. logo" className="logo" />
+      <Link to="./" className="logo">
+        <img src={logo} alt="Around the U.S. logo" />
+      </Link>
       {children}
     </header>
   );
