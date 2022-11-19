9c5f0b0caf1c1c0f81cc4f0246414a6dea11373f
João Fernando <joaofq@hotmail.com>
2022-11-18 21:23:57 -0300

    Add signin.js and fixed form.css

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
 
 function Login(props) {
   return (
-    <>
-      <h1> Hi </h1>
-    </>
+    <main className="main">
+      <Header />
+      <h2 className="form__title">Entrar </h2>
+      <form className="form">
+        <input
+          className="form__input"
+          type="email"
+          placeholder="E-mail"
+          required
+        />
+        <span className="form__error"></span>
+        <input
+          className="form__input"
+          type="password"
+          placeholder="Password"
+          required
+          minLength="8"
+        />
+        <span className="form__error"></span>
+        <button className="form__button" type="submit" aria-labe="Sign Up">
+          Sign up
+        </button>
+        <Link exact to="/signup" className="form__link">
+          Ainda não é membro? Inscreva-se aqui!
+        </Link>
+      </form>
+      <Footer />
+    </main>
   );
 }
 
--- src/components/Register.js
+++ src/components/Register.js
@@ -7,32 +7,28 @@ function Register(props) {
   return (
     <main className="main">
       <Header />
-      <h2 className="register__title">Inscrever-se</h2>
-      <form className="register__form">
+      <h2 className="form__title">Inscrever-se</h2>
+      <form className="form">
         <input
-          className="register__form_input"
+          className="form__input"
           type="email"
           placeholder="E-mail"
           required
         />
         <span className="form__error"></span>
         <input
-          className="register__form_input"
+          className="form__input"
           type="password"
           placeholder="Password"
           required
           minLength="8"
         />
         <span className="form__error"></span>
-        <button
-          className="register__form_button"
-          type="submit"
-          aria-labe="Sign Up"
-        >
+        <button className="form__button" type="submit" aria-labe="Sign Up">
           Sign up
         </button>
-        <Link exact to="/signin" className="register__form_link">
-          Already a member? Log in here!
+        <Link exact to="/signin" className="form__link">
+          Já é um membro? Faça o login aqui!{' '}
         </Link>
       </form>
       <Footer />
